d73a43ea7f7a447b6601218ef284986202e27b9c
Toad Farm <toadfarm@protonmail.com>
2021-03-05 22:28:08 +0100

    Price fix

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,5 +1,5 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0x22D67B3f6aCdf8c0682f6FB20590E902DeEA6BA1',
+  cakeAddress: '0x71F2f0ce6e858de06e94aad9eF0cD4FFFa298034',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
   lpAddress: '0x8C1068F4e72042819106EF914390266e6d484C76'
 }
