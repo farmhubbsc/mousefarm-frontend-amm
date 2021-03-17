a25e6fe788d62fef8ab95f9995910beba478907e
Toad Farm <toadfarm@protonmail.com>
2021-03-17 21:18:59 +0100

    Price indicator fix

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0x71F2f0ce6e858de06e94aad9eF0cD4FFFa298034',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x8C1068F4e72042819106EF914390266e6d484C76'
+  lpAddress: '0x4507e26d4161ac215185e185deb3ef9398ae8fc6'
 }
 
 export default priceContracts
