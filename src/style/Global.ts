cf9c1ffad466b15e631d9e6c8229c7dab607c359
Multichain97 <joshimaahi51@gmail.com>
2021-09-10 23:57:23 -0700

    tell

--- src/style/Global.ts
+++ src/style/Global.ts
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${({ theme }) => theme.colors.background};
-    background: url(https://mouse.farm/images/backgrounds/pattern-v4.png);
+    background: url(https://wallpaperaccess.com/full/51364.jpg);
     img {
       height: auto;
       max-width: 100%;
