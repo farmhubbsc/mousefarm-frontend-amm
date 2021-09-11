29dbadfd9040484138be568f0035ff9c2a2ea920
Multichain97 <joshimaahi51@gmail.com>
2021-09-11 03:14:58 -0700

    frrr

--- src/style/Global.ts
+++ src/style/Global.ts
@@ -4,7 +4,7 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${({ theme }) => theme.colors.background};
-    background: url(https://wallpaperaccess.com/full/1116905.jpg);
+    background: url(https://wallpaperaccess.com/full/1116882.jpg);
     img {
       height: auto;
       max-width: 100%;
