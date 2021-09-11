90206e325af85417f999cf9345fd8549910a9d5a
Multichain97 <joshimaahi51@gmail.com>
2021-09-11 03:08:10 -0700

    FG

--- src/style/Global.ts
+++ src/style/Global.ts
@@ -4,7 +4,7 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${({ theme }) => theme.colors.background};
-    background: url(https://p4.wallpaperbetter.com/wallpaper/911/774/372/black-cubes-wallpaper-preview.jpg);
+    background: url(https://wallpaperaccess.com/full/1116905.jpg);
     img {
       height: auto;
       max-width: 100%;
