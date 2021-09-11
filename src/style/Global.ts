b487fd4a09537b6329d4a959380684178a7b1902
Multichain97 <joshimaahi51@gmail.com>
2021-09-11 03:05:13 -0700

    bg

--- src/style/Global.ts
+++ src/style/Global.ts
@@ -4,7 +4,7 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${({ theme }) => theme.colors.background};
-    background: url(https://wallpaperaccess.com/full/51364.jpg);
+    background: url(https://p4.wallpaperbetter.com/wallpaper/911/774/372/black-cubes-wallpaper-preview.jpg);
     img {
       height: auto;
       max-width: 100%;
